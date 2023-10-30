b7cd48ac0a2fdec8dd6ab9032257f96cef816700
Zachary Hanson <hansonz@purdue.edu>
2023-10-29 23:12:37 -0400

    Done for the day

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+var buildingID = 0;
+
 function nextPage() {
     if (document.getElementById("Choices").value == "Edit") {
         window.location.href = "edit.html";
@@ -10,5 +12,28 @@ function nextPage() {
     else if (document.getElementById("Choices").value == "Delete") {
         window.location.href = "Delete.html";
         return;
+    }
+}
+
+function buildingSelector() {
+    if (document.getElementById("building").value == "Freida") {
+        buildingID = 1;
+    }
+    else if (document.getElementById("building").value == "Hilly") {
+        buildingID = 2;
+    }
+    else if(document.getElementById("building").value == "Tark") {
+        buildingID = 3;
+    }
+}
+
+function userIDCheck(userID) {
+    const Http = new XMLHttpRequest();
+    const url='https://us-central1-cs348project1-403521.cloudfunctions.net/UserIDCheck';
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+    console.log(Http.responseText)
     }
 }
