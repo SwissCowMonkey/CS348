0b034093e47a343acee88ff6de1285ad390d8683
Zachary Hanson <hansonz@purdue.edu>
2023-10-30 13:38:02 -0400

    made some changes

---
+++ cloudFunctions/main.py
@@ -0,0 +1,65 @@
+import mysql.connector
+
+def UserIDCheck(request):
+  """Calls the get_customer_info stored procedure."""
+  userID = request.args.get('userID')
+  headers = {
+        'Access-Control-Allow-Origin': '*',  # Set the appropriate origin if not all origins are allowed
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+    }
+  if request.method == 'OPTIONS':
+        # For preflight requests
+        return ('', 204, headers)
+
+  # Connect to the MySQL database.
+  conn = mysql.connector.connect(host='35.222.78.138',
+                                  user='root',
+                                  database='db1')
+  # Create a cursor.
+  cursor = conn.cursor()
+  # Execute the stored procedure.
+  cursor.callproc('getUser', (userID,))
+
+  # Get the results of the stored procedure.
+  c_results = cursor.stored_results()
+  for c_result in c_results:
+    results = c_result.fetchall()
+  # Close the cursor and connection.
+  cursor.close()
+  conn.close()
+
+  # Return the results of the stored procedure.
+  return (results, 200, headers)
+
+def getAllRooms(request):
+  """Calls the get_customer_info stored procedure."""
+  bID = request.args.get('bID')
+  headers = {
+        'Access-Control-Allow-Origin': '*',  # Set the appropriate origin if not all origins are allowed
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+    }
+  if request.method == 'OPTIONS':
+        # For preflight requests
+        return ('', 204, headers)
+
+  # Connect to the MySQL database.
+  conn = mysql.connector.connect(host='35.222.78.138',
+                                  user='root',
+                                  database='db1')
+  # Create a cursor.
+  cursor = conn.cursor()
+  # Execute the stored procedure.
+  cursor.callproc('getAllRooms', (bID,))
+
+  # Get the results of the stored procedure.
+  c_results = cursor.stored_results()
+  for c_result in c_results:
+    results = c_result.fetchall()
+  # Close the cursor and connection.
+  cursor.close()
+  conn.close()
+
+  # Return the results of the stored procedure.
+  return (results, 200, headers)
--- functions.js
+++ functions.js
@@ -1,5 +1,10 @@
-var buildingID = 0;
+var buildingID = 0; // Set default value if not found
 
+function saveVariable(num) {
+    // Update the variable's value and store it in localStorage
+    buildingID = num; // Change this to your updated value
+    localStorage.setItem("buildingID", buildingID);
+}
 function nextPage() {
     if (document.getElementById("Choices").value == "Edit") {
         window.location.href = "edit.html";
@@ -17,23 +22,88 @@ function nextPage() {
 
 function buildingSelector() {
     if (document.getElementById("building").value == "Freida") {
-        buildingID = 1;
+        console.log("FREIDA");
+        saveVariable(1);
     }
     else if (document.getElementById("building").value == "Hilly") {
-        buildingID = 2;
+        console.log("HILLY");
+        saveVariable(2);
     }
     else if(document.getElementById("building").value == "Tark") {
-        buildingID = 3;
+        console.log("TARK");
+        saveVariable(3);
     }
 }
 
-function userIDCheck(userID) {
-    const Http = new XMLHttpRequest();
-    const url='https://us-central1-cs348project1-403521.cloudfunctions.net/UserIDCheck';
-    Http.open("GET", url);
-    Http.send();
 
-    Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
+function userIDCheck() {
+    var xhr = new XMLHttpRequest();
+    console.log(document.getElementById("userID").value);
+    var userID = document.getElementById("userID").value;
+    var url = 'https://us-central1-cs348project1-403521.cloudfunctions.net/UserIDCheck?userID=' + document.getElementById("userID").value;
+    xhr.open('GET', url, true);
+
+    xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+        // Handle successful response
+        var arr = xhr.responseText.split(",");
+        for (var i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].replace(/[^\w\s]/gi, '')
+            arr[i] = arr[i].replace(/\s/g, '')
+        }
+        if (arr[0] == String(userID)) {
+            if (arr[4] === 'null') {
+                window.location.href = "add3.html";
+            }
+            else if (arr[4] != 'null'){
+                alert("User ID already has a room! Please select another ID")
+            }
+        } else {
+            alert("User ID does not exist");
+        }
+        } else {
+        // Handle error
+        console.error('Request failed with status:', xhr.status);
+        }
     }
+    };
+    xhr.send();
+}
+
+
+function addRows() {
+    var xhr = new XMLHttpRequest();
+    buildingID = localStorage.getItem("buildingID");
+    var url = 'https://us-central1-cs348project1-403521.cloudfunctions.net/getAllRooms?bID=' + buildingID;
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+        // Handle successful response
+        var arr = xhr.responseText.split(",");
+        for (var i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].replace(/[^\w\s]/gi, '')
+            arr[i] = arr[i].replace(/\s/g, '')
+        }
+        console.log(arr);
+        } else {
+        // Handle error
+        console.error('Request failed with status:', xhr.status);
+        }
+    }
+    };
+    
+    xhr.send();
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(table.rows.length - 1); // Insert before the last row (excluding the header row)
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+
+    cell1.innerHTML = 'New Data 1';
+    cell2.innerHTML = 'New Data 2';
+    cell3.innerHTML =  'New Data 3';
+    cell4.innerHTML =  'New Data 4';
 }
